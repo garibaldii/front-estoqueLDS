328610723f065371d6c5070e815db9957b968acd
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-04-17 16:45:27 -0300

    ✨ feat: add SVG icons for Entrada, Estoque, Historico, and Saida

---
+++ public/estoqueIcon.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+const EstoqueIcon = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg width={60} height={60} viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path d="M9.78271 14.6739H3.26099V16.3043H9.78271V14.6739Z" fill="black"/>
+    <path d="M19.0217 11.413V0H5.43481V11.413H0V25H24.4565V11.413H19.0217ZM7.06523 1.63042H17.3913V11.413H7.06523V1.63042ZM11.413 23.3696H1.63047V13.0435H11.4131L11.413 23.3696ZM22.8261 23.3696H13.0435V13.0435H22.8261V23.3696H22.8261Z" fill="black"/>
+    <path d="M21.1956 14.6739H14.6738V16.3043H21.1956V14.6739Z" fill="black"/>
+    <path d="M15.7608 3.26089H8.69556V4.89131H15.7608V3.26089Z" fill="black"/>
+    </svg>
+  );
+};
+
+export default EstoqueIcon;
