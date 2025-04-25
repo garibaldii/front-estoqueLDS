66fa8b7c44e5c0138f8cfee82caebe3d3e6b055b
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-04-25 16:26:07 -0300

    ✨ feat: implement modal for viewing order materials in OrderDataTable component

--- src/components/molecules/Modal.tsx
+++ src/components/molecules/Modal.tsx
@@ -3,19 +3,19 @@ import { Button } from "../ui/button"
 type Props = {
     onClose: () => void
     title: string
-    description: string
+    description: any
 }
 
 export const Modal = ({ onClose, title, description }: Props) => {
     return (
-        <div className="fixed inset-0 bg-black bg-opacity-60 z-50 flex items-center justify-center">
-            <div className="bg-white rounded-2xl max-w-md w-full p-6 text-center shadow-lg">
+        <div className="fixed inset-0 bg-black bg-opacity-60 z-50 flex items-center justify-center p-4 w-full">
+            <div className="bg-white rounded-2xl w-full max-w-3xl p-6 text-center shadow-lg overflow-auto">
                 <div className="bg-gray-500 text-white rounded-t-xl py-2 text-lg font-semibold">
                     {title}
                 </div>
 
-                <div className="mt-6 mb-4 flex items-center justify-center gap-2 text-black">
-                    <span>{description}</span>
+                <div className="mt-6 mb-4 flex flex-wrap items-center justify-center gap-2 text-black break-words">
+                    <span className="max-w-full">{description}</span>
                 </div>
 
                 <div className="flex justify-center mt-6">
--- src/components/organisms/datatable/_components/orders-datatable.tsx
+++ src/components/organisms/datatable/_components/orders-datatable.tsx
@@ -1,41 +1,76 @@
 'use client'
 
 import { ColumnDef } from "@tanstack/react-table"
+import { useMemo, useState } from "react";
+
+import { DataTable } from "@/components/ui/datatable"
+import { Button } from "@/components/ui/button";
+import { Modal } from "@/components/molecules/Modal";
 
 import { Order } from "../_interfaces/order";
-import { DataTable } from "@/components/ui/datatable"
-
-import { ArrowUpDown } from "lucide-react"
-
-const columns: ColumnDef<Order>[] = [
-    {
-        accessorKey: "codigoPedido",
-        header: "Código do Pedido",
-        cell: ({ row }) => {
-            return <div>{row.getValue("codigoPedido")}</div>
-        }
-    },
-    {
-        accessorKey: "listaProdutos",
-        header: "Conteúdo do Pedido",
-        cell: ({ row }) => {
-            return <div>{row.getValue("listaProdutos[0]")}</div>
-        }
-    },
-    {
-        accessorKey: "dataSaida",
-        header: "Data/Hora de Separação",
-        cell: ({ row }) => {
-            return <div>{row.getValue("dataSaida")}</div>
-        }
-
-    }
-]
+import { groupByKeys } from "@/utils/filters";
+import { ProductTable } from "@/components/molecules/ProductTable";
 
 interface Props {
     orders: Order[]
 }
 
-export default function OrderDataTable({ orders: orders }: Props) {
-    return <DataTable columns={columns} data={orders} pageSize={10} searchFields={['codigoPedido', 'dataSaida']} />
+export default function OrderDataTable({ orders }: Props) {
+    const [selectedOrder, setSelectedOrder] = useState<Order | null>(null)
+
+
+    //memoriza em memória cache apenas uma vez as colunas, e as renderiza caso tenha mudança no pedido selecionado
+    const columns = useMemo<ColumnDef<Order>[]>(() => [
+        {
+            accessorKey: "codigoPedido",
+            header: "Código do Pedido",
+            cell: ({ row }) => {
+                return <div>{row.getValue("codigoPedido")}</div>
+            }
+        },
+        {
+            accessorKey: "listaProdutos",
+            header: "Conteúdo do Pedido",
+            cell: ({ row }) => {
+                const order = row.original as Order
+                return (
+                    <Button
+                        variant="ghost"
+                        onClick={() => setSelectedOrder(order)}
+                    >
+                        Ver Materiais
+                    </Button>
+                )
+            }
+        },
+        {
+            accessorKey: "dataSaida",
+            header: "Data/Hora de Separação",
+            cell: ({ row }) => {
+                return <div>{row.getValue("dataSaida")}</div>
+            }
+        }
+    ], [setSelectedOrder])
+
+
+    return (
+        <>
+            <DataTable
+                columns={columns}
+                data={orders}
+                pageSize={10}
+                searchFields={['codigoPedido', 'dataSaida']}
+            />
+
+            {selectedOrder && (
+                <Modal
+                    title={selectedOrder.codigoPedido}
+                    description={
+                        <ProductTable localData={selectedOrder.listaProdutos}/>
+                    }
+                    onClose={() => setSelectedOrder(null)}
+                />
+            )}
+        </>
+    )
 }
