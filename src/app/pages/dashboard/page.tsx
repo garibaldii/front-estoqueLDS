298e1ede28c5e11d3dcb40958e179d4e74f42117
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-04-25 09:33:24 -0300

    ♻️ refactor: enhance Dashboard component; integrate panel data refresh and improve loading state handling

--- src/app/pages/dashboard/page.tsx
+++ src/app/pages/dashboard/page.tsx
@@ -5,27 +5,32 @@ import { NavBar } from "../../../components/organisms/NavBar"
 
 import { usePanel } from "@/context/PanelContext";
 import { useInverter } from "@/context/InverterContext";
+import { useEffect } from "react";
 
- const Dashboard = () => {
-   
-    const {inverters, loading} = useInverter()
-    const {panels} = usePanel()
+const Dashboard = () => {
+    const { inverters, loading } = useInverter()
+    const { panels, refreshPanelData: refreshData } = usePanel()
 
-
-    if (loading) return <p className="text-center mt-10"> Carregando...</p>
+    useEffect(() => {refreshData()}, [])
 
     return (
         <div>
             <NavBar />
 
-            <div className="flex justify-center items-center">
-                <Graph
-                    data={panels}
-                />
-                <Graph
-                    data={inverters}
-                />
-            </div>
+            {loading ? (<p className="text-center mt-10"> Carregando...</p>) : (
+                <div className="flex justify-center items-center">
+
+
+                    <Graph
+                        data={panels}
+                    />
+                    <Graph
+                        data={inverters}
+                    />
+                </div>
+
+            )}
+
 
         </div>
     )
