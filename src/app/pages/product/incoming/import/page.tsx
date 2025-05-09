8669bd713f708a5a7e3914afe56301aafdb2def0
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-05-09 17:16:45 -0300

    ✨ feat: enhance Import page with file upload functionality and modals for user feedback

--- src/app/pages/product/incoming/import/page.tsx
+++ src/app/pages/product/incoming/import/page.tsx
@@ -1,23 +1,81 @@
 'use client'
 
+import { useRouter } from "next/navigation"
+import { useState } from "react"
+
+import Image from "next/image"
+
 import { NavBar } from "@/components/organisms/NavBar"
 import { Button } from "@/components/ui/button"
-import Image from "next/image"
+import { Input } from "@/components/ui/input"
+import { ModalShadcn } from "@/components/molecules/ConfirmationModal"
 import ArrowBack from "../../../../../../public/arrowBack"
 
+import { postFile } from "@/services/file"
+import ProductDataTable from "@/components/organisms/datatable/_components/product-datatable"
+import { groupByKeys } from "@/utils/filters"
+
+
 const Import = () => {
+
+    const router = useRouter()
+
+    const [confirmationModal, setConfirmationModal] = useState(false)
+    const [successModal, setSuccessModal] = useState(false)
+    const [errorModal, setErrorModal] = useState(false)
+
+    const [error, setError] = useState<any>([])
+
+    const [file, setFile] = useState<File | null>(null)
+    const [importedProducts, setImportedProducts] = useState<any>([])
+
+
+
+
+    const handleDownloadFile = () => {
+        const link = document.createElement("a")
+        link.href = `${process.env.NEXT_PUBLIC_API_URL}/file/download`
+        document.body.appendChild(link)
+        link.click()
+        document.body.removeChild(link)
+    }
+
+    const handleSubmitFile = async () => {
+        try {
+            if (!file) return alert("Nenhum arquivo selecionado, tente novamente")
+            const result = await postFile(file)
+
+            if (result) {
+                setConfirmationModal(false)
+                setSuccessModal(true)
+
+                //dados dos produtos do excel
+                setImportedProducts(result.response)
+                console.log(result.response)
+                return
+            }
+            return
+        } catch (error: any) {
+            setError(error)
+            alert(error.response.data.message)
+            console.log(error)
+            setErrorModal(true)
+        }
+    }
+
+
     return (
         <div className="flex flex-col h-screen">
             <NavBar />
 
             <div className="flex flex-col items-center">
-                <div className="flex-1 p-10 flex flex-col  justify-center w-[90%]">
+                <div className="flex-1 p-10 flex flex-col  justify-center w-full">
 
                     <div className="flex">
-                        <div className=" hover:bg-black">
-                            <ArrowBack width={30} />
+                        <div>
+                            <ArrowBack width={30} className="rounded-full  transition-colors duration-300 cursor-pointer hover:bg-slate-50 " onClick={() => router.back()} />
                         </div>
-                        <p className="ml-2 mb-6 text-lg hover:underline hover:cursor-pointer">Inventário de Produtos</p>
+                        <p className="ml-2 mb-6 text-lg hover:underline hover:cursor-pointer" onClick={() => router.back()}>Produtos</p>
                     </div>
                     <h1 className="font-bold text-3xl mb-6 text-gray-700 ">Importar Planilha</h1>
 
@@ -31,7 +89,10 @@ const Import = () => {
                             <div className="mb-10">
                                 <p>· Primeiro Passo:</p>
                                 <p className="mb-2">Clique no botão de baixar planilha modelo abaixo e preencha os dados.</p>
-                                <Button className="w-2/5 p-6 text-lg bg-[#0086FF] hover:bg-[#3181c7]">
+                                <Button
+                                    className="w-2/5 p-6 text-lg bg-[#0086FF] hover:bg-[#3181c7]"
+                                    onClick={() => handleDownloadFile()}
+                                >
                                     Baixar planilha modelo
                                 </Button>
                             </div>
@@ -39,7 +100,10 @@ const Import = () => {
                             <div className="mb-8">
                                 <p>· Segundo Passo:</p>
                                 <p className="mb-2">Importe a planilha com os dados que você preencheu.</p>
-                                <Button className="w-2/5 p-6 text-lg bg-green-600 hover:bg-green-700">
+                                <Button
+                                    className="w-2/5 p-6 text-lg bg-green-600 hover:bg-green-700"
+                                    onClick={() => setConfirmationModal(true)}
+                                >
                                     Importar planilha
                                 </Button>
                             </div>
@@ -54,6 +118,81 @@ const Import = () => {
                                 className="object-cover"
                             />
                         </div>
+
+
+
+                        {confirmationModal && (
+                            <ModalShadcn
+                                open={true}
+                                onClose={() => setConfirmationModal(false)}
+                                onSubmit={() => handleSubmitFile()}
+                                title={"Subir planilha para o estoque"}
+                                description={
+                                    <div className="flex flex-col items-center gap-4">
+                                        <p>Baixe a <b>planilha modelo</b> e carregue com os dados preenchidos
+                                        </p>
+                                        <Input
+                                            type="file"
+                                            accept=".xlsx,.xls"
+                                            className="mb-10 h-20 flex cursor-pointer hover:bg-gray-100"
+                                            onChange={(e) => {
+                                                const selectedFile = e.target.files?.[0]
+                                                if (selectedFile) setFile(selectedFile)
+                                            }}
+                                        />
+
+                                    </div>}
+                            />
+                        )}
+
+                        {successModal && (
+                            <ModalShadcn
+                                open={true}
+                                onClose={() => setSuccessModal(false)}
+                                onSubmit={() => router.push("/pages/product")}
+                                title={"Produtos Importados com Sucesso! 🎉🥳"}
+                                description={
+
+                                    <div className="flex gap-4  max-w-6xl">
+                                        <div>
+                                            <h1>Painéis</h1>
+                                            {/* filtra os dados para que sejam exibidos os produtos cadastrados de forma agrupada, ao invés de mostrar de forma solta */}
+                                            <ProductDataTable products={groupByKeys(importedProducts.panelData, ["marca", "modelo", "potencia",])} />
+                                        </div>
+
+                                        <div>
+                                            <h1>Inversores</h1>
+                                            <ProductDataTable products={groupByKeys(importedProducts.inverterData, ["marca", "modelo", "potencia"])} />
+                                        </div>
+
+                                    </div>}
+
+                            />
+                        )}
+
+                        {errorModal && (
+                            <ModalShadcn
+                                open={true}
+                                onClose={() => setErrorModal(false)}
+                                onSubmit={() => setErrorModal(false)}
+                                title={`${error.response.data.message} ❌`}
+                                description={
+                                    <div className="flex flex-col gap-2 max-w-6xl">
+                                        {error.response.data.info.map((erro: any, index: number) => (
+                                            <div key={index} className="p-2 border rounded bg-red-50">
+                                                {Object.entries(erro).map(([chave, valor]) => (
+                                                    <p key={chave}>
+                                                        <strong>{chave}:</strong> {valor}
+                                                    </p>
+                                                ))}
+                                            </div>
+                                        ))}
+                                    </div>
+                                }
+                            />
+                        )}
+
+
                     </div>
                 </div>
             </div>
