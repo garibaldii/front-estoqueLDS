547f4e64d61ab2c48d36542ab4572f6520b2421e
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-05-16 17:11:34 -0300

    ♻️ refactor: update getUser function to accept user ID instead of email

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,8 +1,9 @@
 import { api } from "./http"
 
-export const getUser = async (email: string) => {
-    const response = await api.get(`/usuario/${email}`)
+
+export const getUser = async (id: string) => {
+    const response = await api.get(`/usuario/${id}`)
     const data = response.data
-    
+
     return data
 }
