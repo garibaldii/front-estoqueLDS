0cded575c10906fb32750967e5a391b7e2ae3ecf
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-05-16 17:08:24 -0300

    ✨ feat: remove Order and Product interfaces as part of code cleanup

--- src/components/organisms/datatable/_interfaces/order.ts
+++
@@ -1,5 +0,0 @@
-export interface Order{
-    codigoPedido: string,
-    listaProdutos: object[]
-    dataSaida: string
-}
--- src/components/organisms/datatable/_interfaces/product.ts
+++
@@ -1,6 +0,0 @@
-export interface Product {
-    marca: string,
-    modelo: string,
-    potencia: number,
-    quantidade: number
-}
